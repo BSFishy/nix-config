bf8132c41c224ad7f62affe1dc59327f102680fd
Matt Provost <mattprovost6@gmail.com>
2024-05-11 18:10:44 -0700

    Add starship config
    Signed-off-by: Matt Provost <mattprovost6@gmail.com>

--- modules/starship.nix
+++ modules/starship.nix
@@ -1,10 +1,39 @@
 { ... }:
 
 {
-  # TODO: install configuration
-
   programs.starship = {
     enable = true;
     enableZshIntegration = true;
+
+    settings = {
+      add_newline = true;
+
+      character = {
+        success_symbol = "[\u276F](bold green)";
+        error_symbol = "[\u2717](bold red)";
+      };
+
+      directory = {
+        format = "[$read_only]($read_only_style)[$path]($style) ";
+        truncation_length = 8;
+      };
+
+      dotnet = {
+        format = "[$symbol($version )]($style)";
+      };
+
+      hostname = {
+        ssh_only = false;
+      };
+
+      sudo = {
+        disabled = false;
+      };
+
+      username = {
+        format = "[$user]($style) on ";
+        show_always = true;
+      };
+    };
   };
 }
