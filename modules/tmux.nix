6c793c699945c221bfdd522a6baed8870b578e28
Matt Provost <mattprovost6@gmail.com>
2024-05-11 21:35:41 -0700

    Update tmux config
    Signed-off-by: Matt Provost <mattprovost6@gmail.com>

--- modules/tmux.nix
+++ modules/tmux.nix
@@ -1,8 +1,28 @@
-{ pkgs, ... }:
+{ config, ... }:
 
 {
-  # TODO: install tmux config
-  home.packages = [
-    pkgs.tmux
-  ];
+  programs.tmux = {
+    enable = true;
+
+    terminal = "tmux-256color";
+    escapeTime = 10;
+    mouse = true;
+    keyMode = "vi";
+    baseIndex = 1;
+
+    extraConfig = ''
+      # Color support
+      set -ga terminal-overrides ",xterm-256color:Tc"
+
+      # For Neovim
+      set-option -g focus-events on
+
+      # Reload config file with prefix R
+      bind r source-file ${config.xdg.configHome}/tmux/tmux.conf \; display-message "Config reloaded.."
+
+      # Vi-like bindings for copy mode
+      bind-key -T copy-mode-vi 'v' send -X begin-selection
+      bind -T copy-mode-vi y send-keys -X copy-pipe "xclip -selection clipboard"
+    '';
+  };
 }
