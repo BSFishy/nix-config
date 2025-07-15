a3bd828e1814fc53a43f63792847b97d7f7765bd
Matt Provost <mattprovost6@gmail.com>
2025-07-15 11:50:45 -0500

    feat: add longhorn
    this is actually a few changes, but primarily getting longhorn set up
    
    Signed-off-by: Matt Provost <mattprovost6@gmail.com>

--- modules/nixos/k8s/k3s.nix
+++ modules/nixos/k8s/k3s.nix
@@ -1,21 +1,47 @@
-{ ... }:
+{ config, pkgs, ... }:
 
 {
   # allow the port for local changes
   networking.firewall.allowedTCPPorts = [ 6443 ];
 
+  # for longhorn
+  environment.systemPackages = [ pkgs.nfs-utils ];
+  services.openiscsi = {
+    enable = true;
+    name = "${config.networking.hostName}-initiatorhost";
+  };
+
   services.k3s = {
     enable = true;
     clusterInit = true;
+    gracefulNodeShutdown.enable = true;
 
     extraFlags = [
       "--disable traefik"
       "--disable servicelb"
+      "--disable coredns"
     ];
 
     manifests = {
       kube-vip.source = ./manifests/kube-vip.yaml;
       kube-vip-cloud-controller.source = ./manifests/kube-vip-cloud-controller.yaml;
+
+      # fix for longhorn paths
+      longhorn-fix.source = ./manifests/longhorn-fix.yaml;
+    };
+
+    autoDeployCharts = {
+      longhorn = {
+        name = "longhorn";
+        createNamespace = true;
+        targetNamespace = "longhorn-system";
+        repo = "https://charts.longhorn.io";
+        version = "1.9.0";
+        hash = "sha256-xQEQ3Od9EZnGxr2levfQpKgh3Qinet9xhiNM4twALtw=";
+
+        # TODO: i would really like this to be a yaml file
+        values = import ./longhorn.nix { };
+      };
     };
   };
 }
