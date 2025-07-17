f5e6de24eaa221790af7ef2a2e8f3675d3b5cab3
Matt Provost <mprovost@cloudflare.com>
2025-07-16 21:46:46 -0500

    chore: iuberoifa
    Signed-off-by: Matt Provost <mprovost@cloudflare.com>

--- modules/home-manager/basic/user.nix
+++ modules/home-manager/basic/user.nix
@@ -1,11 +1,17 @@
-{ pkgs, username, ... }:
+{
+  config,
+  pkgs,
+  username,
+  ...
+}:
 
 let
   isLinux = pkgs.stdenv.hostPlatform.isLinux;
+  value = builtins.trace "Final home.homeDirectory: ${toString config.home-manager.users.mprovost.home.homeDirectory}" "/Users/mprovost";
 in
 {
   # set basic home configuration
-  home.username = username;
+  home.username = builtins.trace value username;
   home.homeDirectory = builtins.trace "home directory" (
     if isLinux then "/home/${username}" else "/Users/${username}"
   );
