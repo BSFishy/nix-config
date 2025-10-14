da3d752d661bee983f1d181d6d61556d22196399
Matt Provost <mattprovost6@gmail.com>
2025-10-14 10:28:41 -0500

    feat: add delta and difftastic for git diffs
    Signed-off-by: Matt Provost <mattprovost6@gmail.com>

--- modules/home-manager/utilities/git.nix
+++ modules/home-manager/utilities/git.nix
@@ -1,6 +1,20 @@
-{ work, ... }:
+{ pkgs, work, ... }:
 
+let
+  difftastic = pkgs.difftastic;
+  delta = pkgs.delta;
+
+  delta-git = builtins.fetchGit {
+    url = "https://github.com/dandavison/delta.git";
+    rev = "c22adcea90a5f3de2419a058fb92f1ced2a9fd10";
+  };
+in
 {
+  home.packages = [
+    difftastic
+    delta
+  ];
+
   programs.git = {
     enable = true;
     lfs.enable = true;
@@ -8,9 +22,36 @@
     userName = "Matt Provost";
     userEmail = if work then "mprovost@cloudflare.com" else "mattprovost6@gmail.com";
 
+    includes = [
+      { path = "${delta-git}/themes.gitconfig"; }
+    ];
+
     extraConfig = {
       init.defaultBranch = "main";
       core.autocrlf = "input";
+
+      # aliases
+      alias = {
+        dft = "-c diff.external=${difftastic}/bin/difft diff";
+      };
+
+      # pager
+      pager =
+        let
+          delta-bin = "${delta}/bin/delta";
+        in
+        {
+          diff = delta-bin;
+          show = delta-bin;
+          log = delta-bin;
+        };
+
+      # delta diff tool
+      delta = {
+        side-by-side = true;
+        line-numbers = true;
+        # syntax-theme = "gruvmax-fang";
+      };
 
       # merge tool
       merge.tool = "nvimdiff";
