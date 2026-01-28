f1f1395209cc4f57f96ea9d9dfb634963fc455f7
Matt Provost <mprovost@cloudflare.com>
2026-01-28 17:03:35 +0000

    chore: disable homebrew auto update
    holy shit this is so annoying
    
    Signed-off-by: Matt Provost <mprovost@cloudflare.com>

--- modules/nix-darwin/homebrew/default.nix
+++ modules/nix-darwin/homebrew/default.nix
@@ -1,7 +1,18 @@
-{ ... }:
+_:
 
 {
   homebrew = {
     enable = true;
+
+    global = {
+      autoUpdate = false;
+      brewfile = true;
+      lockfiles = true;
+    };
+
+    onActivation = {
+      autoUpdate = false;
+      cleanup = "none";
+    };
   };
 }
