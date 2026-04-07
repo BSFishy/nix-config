84b4cd8fbeb3db7b4ef8f9fd33ee1ff1c93aa67b
Matt Provost <mprovost@cloudflare.com>
2026-04-07 19:41:12 +0000

    chore: misc changes
    Signed-off-by: Matt Provost <mprovost@cloudflare.com>

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,7 +1,8 @@
 let
   matt = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGOo7iBDgCXP99GA4NStJudsWkZQVaA9iDqDo6IQF2ve";
+  work = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFfD9CzSpDp1VNx/ciax6PMJJ5WFCIeR1ogoI4HXadz8";
 in
 {
-  "github-mcp-pat.age".publicKeys = [ matt ];
+  "github-mcp-pat.age".publicKeys = [ matt work ];
   "k3s-token.age".publicKeys = [ matt ];
 }
